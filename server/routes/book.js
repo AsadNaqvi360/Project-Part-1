6beb61248d14ec2c25a5a8b7c8029d9c3a06683e
Ahhmedd23 <ahmedabdullahbaghabra.alamoodi@ontariotechu.net>
2024-11-12 16:16:43 -0500

    Updated the booklist
    Updated the booklist to Display the inventory list

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,122 +1,108 @@
 var express = require('express');
 var router = express.Router();
 let mongoose = require('mongoose');
-// telling my router that I have this model
-let Book = require('../model/book');
-const book = require('../model/book');
-let bookController = require('../controllers/book.js')
-/* Get route for the book list - Read Operation */
-/*
-GET,
-Post,
-Put --> Edit/Update
-*/
-/* Read Operation --> Get route for displaying the books list */
-router.get('/',async(req,res,next)=>{
-try{
-    const BookList = await Book.find();
-    res.render('Book/list',{
-        title:'Books',
-        BookList:BookList
-    })}
-    catch(err){
-        console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
-    }
-    });
-/* Create Operation --> Get route for displaying me the Add Page */
-router.get('/add',async(req,res,next)=>{
-    try{
-        res.render('Book/add',{
-            title: 'Add Book'
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
-    }
-});
-/* Create Operation --> Post route for processing the Add Page */
-router.post('/add',async(req,res,next)=>{
-    try{
-        let newBook = Book({
-            "Name":req.body.Name,
-            "Author":req.body.Author,
-            "Published":req.body.Published,
-            "Description":req.body.Description,
-            "Price":req.body.Price
+let Item = require('../model/item'); // Updated model import to 'item'
+
+/* Read Operation: Get route for displaying the inventory list */
+router.get('/', async (req, res, next) => {
+    try {
+        const ItemList = await Item.find();
+        res.render('Inventory/list', {
+            title: 'Inventory',
+            ItemList: ItemList
         });
-        Book.create(newBook).then(()=>{
-            res.redirect('/bookslist');
-        })
-    }
-    catch(err)
-    {
+    } catch (err) {
         console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
+        res.render('Inventory/list', {
+            error: 'Error retrieving inventory data'
+        });
     }
 });
-/* Update Operation --> Get route for displaying me the Edit Page */
-router.get('/edit/:id',async(req,res,next)=>{
-    try{
+
+/* Create Operation: Display the Add Item Page */
+router.get('/add', async (req, res, next) => {
+    try {
+        res.render('Inventory/add', {
+            title: 'Add Item'
+        });
+    } catch (err) {
+        console.error(err);
+        res.render('Inventory/list', {
+            error: 'Error on the server'
+        });
+    }
+});
+
+/* Create Operation: Process the Add Item */
+router.post('/add', async (req, res, next) => {
+    try {
+        let newItem = new Item({
+            "ItemName": req.body.ItemName,
+            "Supplier": req.body.Supplier,
+            "DateReceived": req.body.DateReceived,
+            "ItemDescription": req.body.ItemDescription,
+            "Cost": req.body.Cost,
+            "Quantity": req.body.Quantity
+        });
+        await newItem.save();
+        res.redirect('/inventory');
+    } catch (err) {
+        console.error(err);
+        res.render('Inventory/list', {
+            error: 'Error saving new item'
+        });
+    }
+});
+
+/* Update Operation: Display Edit Item Page */
+router.get('/edit/:id', async (req, res, next) => {
+    try {
         const id = req.params.id;
-        const bookToEdit= await Book.findById(id);
-        res.render('Book/edit',
-            {
-                title:'Edit Book',
-                Book:bookToEdit
-            }
-        )
-    }
-    catch(err)
-    {
-        console.error(err);
-        next(err); // passing the error
-    }
-});
-/* Update Operation --> Post route for processing the Edit Page */ 
-router.post('/edit/:id',async(req,res,next)=>{
-    try{
-        let id=req.params.id;
-        let updatedBook = Book({
-            "_id":id,
-            "Name":req.body.Name,
-            "Author":req.body.Author,
-            "Published":req.body.Published,
-            "Description":req.body.Description,
-            "Price":req.body.Price
+        const itemToEdit = await Item.findById(id);
+        res.render('Inventory/edit', {
+            title: 'Edit Item',
+            Item: itemToEdit
         });
-        Book.findByIdAndUpdate(id,updatedBook).then(()=>{
-            res.redirect('/bookslist')
-        })
-    }
-    catch(err){
+    } catch (err) {
         console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
+        next(err);
     }
 });
-/* Delete Operation --> Get route to perform Delete Operation */
-router.get('/delete/:id',async(req,res,next)=>{
-    try{
-        let id=req.params.id;
-        Book.deleteOne({_id:id}).then(()=>{
-            res.redirect('/bookslist')
-        })
-    }
-    catch(error){
+
+/* Update Operation: Process the Edit Item */
+router.post('/edit/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const updatedItem = {
+            "ItemName": req.body.ItemName,
+            "Supplier": req.body.Supplier,
+            "DateReceived": req.body.DateReceived,
+            "ItemDescription": req.body.ItemDescription,
+            "Cost": req.body.Cost,
+            "Quantity": req.body.Quantity
+        };
+        await Item.findByIdAndUpdate(id, updatedItem);
+        res.redirect('/inventory');
+    } catch (err) {
         console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
+        res.render('Inventory/list', {
+            error: 'Error updating item'
+        });
     }
 });
+
+/* Delete Operation */
+router.get('/delete/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await Item.findByIdAndDelete(id);
+        res.redirect('/inventory');
+    } catch (err) {
+        console.error(err);
+        res.render('Inventory/list', {
+            error: 'Error deleting item'
+        });
+    }
+});
+
 module.exports = router;
