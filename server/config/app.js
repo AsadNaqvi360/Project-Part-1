10fe7972d35bdd5a2283cb4b66c00a294af38569
Happy Hiten <hiten.diwan@ontariotechu.net>
2024-11-12 16:36:15 -0500

    Updates
    Updates app,add,edit,list,header,main_nav

--- server/config/app.js
+++ server/config/app.js
@@ -7,31 +7,21 @@ let logger = require('morgan');
 let app = express();
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
-let bookRouter = require('../routes/book');
-
+let inventoryRouter = require('../routes/book'); // Updated router to handle inventory
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
-// getting-started.js
+
+// MongoDB setup
 const mongoose = require('mongoose');
 let DB = require('./db');
-// point mongoose to the DB URI
-mongoose.connect(DB.URI);
+mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let mongoDB = mongoose.connection;
-mongoDB.on('error',console.error.bind(console,'Connection Error'));
-mongoDB.once('open',()=>{
-  console.log("Connected with the MongoDB")
+mongoDB.on('error', console.error.bind(console, 'Connection Error'));
+mongoDB.once('open', () => {
+    console.log("Connected with MongoDB");
 });
-mongoose.connect(DB.URI,{useNewURIParser:true,useUnifiedTopology:true})
-/* main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/BookLib');
-  //await mongoose.connect('mongodb+srv://ahmedsheikh:Test123@cluster0.0f3pz.mongodb.net/');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}*/
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,9 +32,7 @@ app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bookslist',bookRouter);
-// /project --> projectrouter
-// /contactus --> contactus
+app.use('/inventory', inventoryRouter); // Updated path
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,13 +41,10 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error',{title:'Error'});
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
